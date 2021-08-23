c16ee7180d32f5775f7cab67ae86f3bf4be264db
Mike Lee <mike@mlee.tech>
2021-08-23 18:19:20 -0500

    Rewind App Complete

--- js/site.js
+++ js/site.js
@@ -1,4 +1,39 @@
-// Call Hello World
-function helloWorld(){
-    alert("Hello World!");
+//Get string from the UI
+//Controller Function
+function getValue(){
+
+document.getElementById("alert").classList.add("invisible");
+
+let userString = document.getElementById("userString").value;
+
+let revString = reverseString(userString);
+
+displayString(revString);
+
+}
+
+//Reverse the String
+//Logic Function
+function reverseString(userString){
+
+    let revString = [];
+
+    // reverse a string using a for loop
+
+    for (let index = userString.length - 1; index >= 0; index--) {
+        revString.push(userString[index]);
+    }
+
+    return revString.join('');
+
+}
+
+//Display string in message to user
+//View Function
+function displayString(revString){
+    //write to the page
+
+    document.getElementById("msg").innerHTML = `${revString}`;
+    //turn on alert
+    document.getElementById("alert").classList.remove("invisible");
 }
